3c8c8f37f2d47a64b7b6186da7da92799f131bcd
insideTheFlux <dcrin3.films@gmail.com>
2022-08-17 23:33:44 -0700

    Update Generate-Names-Create-Users.ps1
    Change these:
    $fisrtName = generate-random-name
    $username = $fisrtName + '.' + $lastName
    --====--
    To:
    $firstName = generate-random-name
    $username = $firstName + '.' + $lastName
    
    Basically 'firstname' was misspelled.

--- Generate-Names-Create-Users.ps1
+++ Generate-Names-Create-Users.ps1
@@ -26,9 +26,9 @@ Function generate-random-name() {
 
 $count = 1
 while ($count -lt $NUMBER_OF_ACCOUNTS_TO_CREATE) {
-    $fisrtName = generate-random-name
+    $firstName = generate-random-name
     $lastName = generate-random-name
-    $username = $fisrtName + '.' + $lastName
+    $username = $firstName + '.' + $lastName
     $password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
 
     Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
